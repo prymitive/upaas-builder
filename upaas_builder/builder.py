0b3d1f4a136029abf0d32921b3c8d793087d1185
Łukasz Mierzwa <l.mierzwa@gmail.com>
2013-02-24 21:11:01 +0100

    updates

--- upaas_builder/builder.py
+++ upaas_builder/builder.py
@@ -15,45 +15,107 @@ from upaas import distro
 from upaas import commands
 from upaas import tar
 from upaas import config
+from upaas.chroot import Chroot
+from upaas.storage.exceptions import StorageError
 
 from upaas_builder import exceptions
+from upaas_builder.config import BuilderConfig
 
 log = logging.getLogger(__name__)
 
 
-class BuilderConfig(config.Config):
-
-    schema = {
-        "paths": {
-            "workdir": config.FSPathEntry(required=True, must_exist=True),
-        },
-        "storage": {
-            "handler": config.StringEntry(required=True),
-            "settings": {
-                "dir": config.FSPathEntry(required=True, must_exist=True),
-            }
-        },
-        "bootstrap": {
-            "timelimit": config.IntegerEntry(required=True),
-            "env": config.ListEntry(unicode),
-            "commands": config.ScriptEntry(required=True),
-        },
-        "commands": {
-            "install": config.StringEntry(required=True),
-            "uninstall": config.StringEntry(required=True),
-        }
-        #TODO interpreters?
-    }
-
-
 class Builder(object):
 
-    def __init__(self, config_path):
+    action_names = ["before", "main", "after"]
+
+    def __init__(self, config_path, metadata):
         """
         :param config_path: Path to builder configuration.
+        :param metadata: Application metadata.
         """
+        self.metadata = metadata
         self.config = BuilderConfig.from_file(config_path)
+        self.actions = self.parse_actions(metadata)
+        self.envs = self.parse_envs(metadata)
+        self.os_packages = self.parse_packages(metadata)
         self.storage = self.find_storage_handler()
+
+    def parse_actions(self, meta):
+        """
+        Parse and merge all config files (builder and app meta), then return
+        final list of all actions to perform.
+        """
+        ret = {}
+        for name in self.action_names:
+            ret[name] = []
+            for version in ["any"] + meta.interpreter.versions:
+                try:
+                    value = self.config.interpreters[meta.interpreter.type][
+                        version]["actions"]["setup"][name]
+                    if value:
+                        ret[name] = value
+                except KeyError:
+                    pass
+                else:
+                    log.debug(u"Got '%s' action from %s/%s" % (
+                        name, meta.interpreter.type, version))
+            try:
+                value = meta.actions.setup.get(name)
+                if value:
+                    ret[name] = value
+            except AttributeError:
+                pass
+            else:
+                log.debug(u"Got '%s' action from app meta" % name)
+        return ret
+
+    def parse_envs(self, meta):
+        """
+        Parse and merge all config files (builder and app meta), then return
+        final all env variables used in actions.
+        """
+        ret = {}
+        for version in ["any"] + meta.interpreter.versions:
+            try:
+                value = self.config.interpreters[meta.interpreter.type][
+                    version]["env"]
+                if value:
+                    ret.update(value)
+            except KeyError:
+                pass
+            else:
+                log.debug(u"Got env variables from %s/%s: %s" % (
+                    meta.interpreter.type, version,
+                    u", ".join([u"%s=%s" % (k, v) for k, v in value.items()])))
+            if meta.get(u"env"):
+                ret.update(meta.env)
+                log.debug(u"Got env variables from app meta: %s" % u", ".join(
+                    [u"%s=%s" % (k, v) for k, v in value.items()]))
+        return ret
+
+    def parse_packages(self, meta):
+        """
+        Parse and merge all config files (builder and app meta), then return
+        final list of all packages to install.
+        """
+        ret = set()
+        for version in ["any"] + meta.interpreter.versions:
+            try:
+                for pkg in self.config.interpreters[meta.interpreter.type][
+                        version]["packages"]:
+                    ret.add(pkg)
+                    log.debug(u"Will install package '%s' from builder config "
+                              u"for interpreter version '%s'" % (pkg, version))
+            except KeyError:
+                pass
+            try:
+                for pkg in meta.os[distro.distro_name()]["packages"]:
+                    ret.add(pkg)
+                    log.debug(u"Will install package '%s' from metadata for "
+                              u"distribution %s" % (pkg, distro.distro_name()))
+            except KeyError:
+                pass
+        return ret
 
     def find_storage_handler(self):
         """
@@ -83,6 +145,7 @@ class Builder(object):
         """
         Build a package
 
+        :param meta: Package metadata passed as Config instance.
         :param force_fresh: Force fresh package built using empty system image.
         """
         def _cleanup(directory):
@@ -95,11 +158,121 @@ class Builder(object):
             except exceptions.OSBootstrapError:
                 log.error(u"Error during os bootstrap, aborting")
                 raise exceptions.PackageSystemError
+            except StorageError:
+                log.error(u"Error during uploading os image, aborting")
+                raise exceptions.PackageSystemError
 
         directory = tempfile.mkdtemp(dir=self.config.paths.workdir,
                                      prefix="upaas_package_")
+        workdir = os.path.join(directory, "workdir")
+        homedir = os.path.join(workdir, "home/app")
+        os.mkdir(workdir, 0755)
+        log.info(u"Working directory created at '%s'" % workdir)
 
+        if not self.unpack_os(directory, workdir, force_fresh=force_fresh):
+            _cleanup(directory)
+            raise exceptions.PackageSystemError
+        log.info(u"Os image unpacked")
+
+        if not self.install_packages(workdir):
+            _cleanup(directory)
+            raise exceptions.PackageUserError
+        log.info(u"All packages installed")
+
+        if not self.clone(workdir, homedir):
+            _cleanup(directory)
+            raise exceptions.PackageUserError
+        log.info(u"Application cloned")
+
+        if not self.run_actions(workdir, homedir):
+            _cleanup(directory)
+            raise exceptions.PackageUserError
+        log.info(u"All actions executed")
+
+        package_path = os.path.join(directory, "package")
+        if not tar.pack_tar(workdir, package_path):
+            _cleanup(directory)
+            raise exceptions.PackageSystemError
+        log.info(u"Application package created")
+
+        try:
+            #FIXME remote package name?
+            self.storage.put(package_path, "package")
+        except StorageError:
+            log.error(u"Error while uploading package")
+            _cleanup(directory)
+            raise exceptions.PackageSystemError
+
+        _cleanup(directory)
+
+    def unpack_os(self, directory, workdir, force_fresh=False):
         #TODO right now we always build fresh package
+        os_image_path = os.path.join(directory, distro.distro_image_filename())
+        log.info(u"Fetching os image")
+        try:
+            self.storage.get(distro.distro_image_filename(),os_image_path)
+        except StorageError:
+            log.error(u"Storage error while fetching os image")
+            return False
+        else:
+            log.info(u"Unpacking os image")
+            if not tar.unpack_tar(os_image_path, workdir):
+                log.error(u"Error while unpacking os image to '%s'" % workdir)
+                return False
+        return True
+
+    def install_packages(self, workdir):
+        with Chroot(workdir):
+            for name in self.os_packages:
+                cmd = self.config.commands.install.cmd.replace("%package%",
+                                                               name)
+                try:
+                    commands.execute(cmd,
+                                     timeout=self.config.commands.timelimit,
+                                     env=self.config.commands.install.env,
+                                     output_loglevel=logging.INFO)
+                except commands.CommandTimeout:
+                    log.error(u"Installing package '%s' is taking to long, "
+                              u"aborting" % name)
+                    return False
+                except commands.CommandFailed:
+                    log.error(u"Installing package '%s' failed" % name)
+        return True
+
+    def clone(self, workdir, homedir):
+        with Chroot(workdir):
+            for cmd in self.metadata.repository.clone:
+                cmd = cmd.replace("%destination%", homedir)
+                try:
+                    commands.execute(cmd,
+                                     timeout=self.config.commands.timelimit,
+                                     output_loglevel=logging.INFO)
+                except commands.CommandTimeout:
+                    log.error(u"Cloning repository is taking too long, "
+                              u"aborting")
+                    return False
+                except commands.CommandFailed:
+                    log.error(u"Cloning failed")
+                    return False
+        return True
+
+    def run_actions(self, workdir, homedir):
+        for name in self.action_names:
+            log.info(u"Executing '%s' setup actions" % name)
+            for cmd in self.actions[name]:
+                with Chroot(workdir, workdir=homedir):
+                    try:
+                        commands.execute(cmd,
+                                         timeout=self.config.commands.timelimit,
+                                         env=self.envs)
+                    except commands.CommandTimeout:
+                        log.error(u"Command is taking to long to execute, "
+                                  u"aborting")
+                        return False
+                    except commands.CommandFailed:
+                        log.error(u"Command failed")
+                        return False
+        return True
 
     def has_valid_os_image(self):
         """
@@ -126,19 +299,19 @@ class Builder(object):
         log.debug(u"Created temporary directory for bootstrap at "
                   u"'%s'" % directory)
 
-        cmd = self.config.bootstrap.commands.replace("%workdir%", directory)
-        try:
-            commands.execute(cmd, timeout=self.config.bootstrap.timelimit,
-                             cwd=directory,
-                             env=self.config.bootstrap.env or [])
-        except commands.CommandTimeout:
-            log.error(u"Bootstrap was taking too long and it was killed")
-            _cleanup(directory)
-            raise exceptions.OSBootstrapError
-        except commands.CommandFailed:
-            log.error(u"Bootstrap command failed")
-            _cleanup(directory)
-            raise exceptions.OSBootstrapError
+        for cmd in self.config.bootstrap.commands:
+            cmd = cmd.replace("%workdir%", directory)
+            try:
+                commands.execute(cmd, timeout=self.config.bootstrap.timelimit,
+                                 cwd=directory, env=self.config.bootstrap.env)
+            except commands.CommandTimeout:
+                log.error(u"Bootstrap was taking too long and it was killed")
+                _cleanup(directory)
+                raise exceptions.OSBootstrapError
+            except commands.CommandFailed:
+                log.error(u"Bootstrap command failed")
+                _cleanup(directory)
+                raise exceptions.OSBootstrapError
         log.info(u"Bootstrap done, packing image")
 
         archive_path = os.path.join(directory, "image.tar.gz")
---
+++ upaas_builder/config.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+    :copyright: Copyright 2013 by Łukasz Mierzwa
+    :contact: l.mierzwa@gmail.com
+"""
+
+
+from upaas import config
+
+
+class BuilderConfig(config.Config):
+
+    schema = {
+        "paths": {
+            "workdir": config.FSPathEntry(required=True, must_exist=True),
+        },
+        "storage": {
+            "handler": config.StringEntry(required=True),
+            "settings": {
+                "dir": config.FSPathEntry(required=True, must_exist=True),
+            }
+        },
+        "bootstrap": {
+            "timelimit": config.IntegerEntry(required=True),
+            "env": config.DictEntry(value_type=unicode),
+            "commands": config.ScriptEntry(required=True),
+        },
+        "commands": {
+            "timelimit": config.IntegerEntry(required=True),
+            "install": {
+                "env": config.DictEntry(value_type=unicode),
+                "cmd": config.StringEntry(required=True),
+            },
+            "uninstall": {
+                "env": config.DictEntry(value_type=unicode),
+                "cmd": config.StringEntry(required=True),
+            },
+        },
+        "interpreters": config.WildcardEntry(),
+    }
+
+
+class MetadataConfig(config.Config):
+
+    schema = {
+        "os": config.WildcardEntry(),
+        "interpreter": {
+            "type": config.StringEntry(required=True),
+            "versions": config.ListEntry(unicode),
+        },
+        "repository": {
+            "clone": config.ScriptEntry(required=True),
+            "update": config.ScriptEntry(required=True),
+            "info": config.ScriptEntry(required=True),
+            "changelog": config.ScriptEntry(required=True),
+        },
+        "env": config.DictEntry(value_type=unicode),
+        "actions": {
+            "setup": {
+                "before": config.ScriptEntry(),
+                "main": config.ScriptEntry(),
+                "after": config.ScriptEntry(),
+            }
+        }
+    }
